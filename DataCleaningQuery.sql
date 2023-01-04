0711d32fe9ff184c450c303d094e2b328bc24856
PaulinaJohn <johnpaulina1992@gmail.com>
2023-01-04 16:16:53 +0100

    removed .pbix file and added separate document for the syntaxes used in the analysis step

--- DataCleaningQuery.sql
+++ DataCleaningQuery.sql
@@ -1,19 +1,20 @@
--- Social buzz data cleaning script
+-- Social buzz Data cleaning and merging script.
 -- I used MSSQL for this task.
--- This task is strictly about cleaning each of the three datasets provided.
 
+
+-- The first task is strictly about cleaning each of the three datasets provided.
 -- Creating Schema
 CREATE  SCHEMA socialbuzz;
 
-/*Although I wrote a query to create the schema for this task, going forward,
-I will approach the task like I am a junior Data Analyst who has been restricted from using most DDL and DML languages when working with this database, 
+/*Although I wrote a query to create the schema for this task, 
+going forward, I will approach the task like I am a junior Data Analyst who has been restricted from using most DDL and DML languages when working with this database, 
 but I have been asked to fetch a cleaned subset of each of the three datasets that will be needed to execute the requirements from Socialbuzz anyways. 
-Oftentimes, intern and junior analysts are restricted from such administrator privileges and only allowed to use mainly query language and commands,
-so, it helps to get used to how to get the task done in spite of this restriction
+Oftentimes, intern and junior analysts are restricted from such administrator privileges and only allowed to use mainly query language and commands to get tasks done,
+so, it helps to get used to how to achieving what's required without being held back by the restriction.
 This is one way I demonstrate my problem-solving skill and analytical mindset.
 
 There are usually a number of ways to solve these problems.
-However, a simple SELECT statement with a WHERE clause where necessary, works in this case. 
+However, a SELECT statement using a numer of commands to to mop up dirty data issues, coupled with a WHERE clause where necessary, works in this case. 
 I can save the cleaned dataset as a view and export it as a csv file.
 */
 
@@ -33,24 +34,22 @@ SELECT DISTINCT * FROM socialbuzz.ReactionTypes;
 --Cleaning the datasets
 /* From these datasets, we need to
 - remove rows that have null values
-- remove columns that are outrightly redundant- like column1 in all the datasets.
-The column header was auto-generated from a user-defined index in the csv file during import into the database.
-and was not initially so named in the orginal dataset
--Collect out a subset from each dataset, of columns relevant to the task
+- remove columns that are outrightly redundant- like column1 in all the datasets. The header for this column was auto-generated from a user-defined index in the csv file during import into the database.
+and was not initially named in the orginal dataset.
+-Collect out columns relevant to the task from each dataset.
 
-Datatypes were correctly infered at import so, all the columns are in the right datatype
+Datatypes were correctly infered at import so, all the columns are in the right datatype.
 
-In order to avoid creating more than one resulting output and having to query the result again to achieve the final cleaned dataset, 
-I will work towards achieving all the aforelisted cleaning processes in one query for each of the datasets
+To avoid creating more than one resulting output and having to query the result again to achieve the final cleaned dataset, 
+I will work towards achieving all the aforelisted cleaning steps in one query for each of the datasets.
 */
 
 /* Cleaning the Content table
-The main dirty data issues to address here are:
-- removing rows with null values and doing away with column1. 
-I am going to remove the null values without using the 'DELETE' statement- No DDLs and DMLs, remember. I will only use the CREATE statement to create the views in this work
+
+I am going to remove the null values without using the 'DELETE' statement- No DDLs and DMLs, remember. I will only use the CREATE statement to create the final views.
 Then, since I am also going to subset the data for only those columns relevant to the project,
 I will just leave out column1 with the other irrelevant columns as I do this.
-I will also do some data transformation, like renaming some columns to make them better descriptive, where needed.
+I will also do some data transformation, like renaming some columns to make them more descriptive, where needed.
 
 The columns I need from the content dataset are content ID, category, content type.
 */
@@ -61,7 +60,7 @@ The columns I need from the content dataset are content ID, category, content ty
 SELECT DISTINCT Category
 FROM socialbuzz.Content
 
--- There is value inconsistency in the column. Some of the categories have multiple variants due to some values having double quotes.
+-- There is value inconsistency in the column. Some of the categories have multiple variants due to some values starting and ending with double quotes.
 -- Also, there is inconsitency in case for some values. All these, I'll address when subseting the final, cleaned dataset.
 
 -- I also checked the 'Type' columnn
@@ -72,19 +71,19 @@ FROM socialbuzz.Content
 -- Now, getting the cleaned dataset.
 
 CREATE VIEW socialbuzz.CleanedContent AS
---WITH test_cte AS
+
 SELECT Content_ID,  LOWER(REPLACE(Category,'"', '')) AS Category, Type AS Content_Type -- I used the 'REPLACE' function to get rid of the double quotes. 
 --Note that REPLACE is not a DML as it doesn't get rid of the value inconsistency issue in the original dataset. The changes only reflect in the cleaned dataset.
 FROM
 socialbuzz.Content
 --SELECT DISTINCT Category FROM test_cte
 
--- I used 'Script table as' to see the table description, so as to identify columns not constrained as 'Not Null' as these are the columns that could introduce null values.
+-- I used 'Script table as' to see the table description, so as to identify columns not constrained as 'Not Null' as these are the columns that could have null values.
 -- Only the URL column contains null values and we don't need the column itself so, no need to add a WHERE clause to filter for non-null rows.
 -- In a case where there are many columns, use 'Design Query in Editor' to get a list of all the columns, then relevant columns can be copied into the query.
 
 
--- Next, let's work on the 'Reactions' data.
+-- Next, working on the 'Reactions' data.
 -- The columns we need from this data are 'Content_ID' and reaction type and Date- to see if there is a trend in content popularity
 
 -- I checked for value inconsistency in the 'Type'(In this case, reaction type) column.
@@ -97,14 +96,13 @@ FROM socialbuzz.Reactions
 CREATE VIEW socialbuzz.CleanedReactions AS
 
 SELECT Content_ID, Type as Reaction_Type, CAST(Datetime AS Date) AS Date
---LEFT(CONVERT(DATETIME2(0), Datetime), RIGHT(Datetime, 3)) 
 FROM socialbuzz.Reactions
 WHERE Type IS NOT NULL-- This is the only column needed that has null values so, I used it in my WHERE clause.
 
--- Lastly, let's work on the 'ReactionTypes' data.
--- We'll only take out Column1 from this dataset. We'll keep every other column and rename 'Type' as 'Reaction_Type'
+-- Lastly, working on the 'ReactionTypes' data.
+-- I'll only take out Column1 from this dataset. I'll keep every other column then, rename 'Type' as 'Reaction_Type' and 'Score' as 'Reaction_Score'
 
--- Let's check for value inconstency in the Sentiment and Type columns
+-- Let's check for value inconstency in the 'Sentiment' and 'Type' columns.
 SELECT DISTINCT Sentiment
 FROM socialbuzz.ReactionTypes
 
@@ -121,20 +119,20 @@ FROM socialbuzz.ReactionTypes
 
 
 --PS: For the export process, you may want to have a csv file already created and should be empty.
---Exporting a table or view in the MSSQL database used was appending new rows to existing rows in the csv file 
---and did not overide existing rows. I hope to investigate this and find a solution soon. 
+--Exporting a table or view in the MSSQL database used meant appending new rows to existing rows in a csv file 
+--and did not overide existing rows. I hope to investigate this and find a high-level solution soon. 
 --But for now, my low-level solution is to ensure I already have a csv file I am exporting into and it is empty.
 -- You can open the csv file in notepad, for example, clear out existing data, if necessary, and save.
 
 /*
 Next Task.
 The goal of this task is to merge all three datasets into a final dataset that has only the columns that will be used in our analysis.
-The main aim of the project is 
+The main aim of the project is; 
 "An Analysis of Socialbuzz's content categories showing the top 5 categories with the largest popularity".*/
 
 --So, I will do this using 2 Joins, a subquery, a CTE, and save the entire result as a view
 --The final columns we need are content ID, category, content type, reaction type, sentiment, reaction score and Date
--- We will query the views earlier created to achieve this.
+-- I will query the views earlier created to achieve this.
 
 SELECT * FROM socialbuzz.CleanedContent;
 SELECT * FROM socialbuzz.CleanedReactions;
@@ -143,9 +141,9 @@ SELECT * FROM socialbuzz.CleanedReactionTypes;
 CREATE VIEW socialbuzz.merged_socialbuzz_data_data AS -- the view
 WITH final_cte AS -- the Common table expression - CTE
 (SELECT r.Content_ID, c.Category, c.Content_Type, r.Reaction_Type,  r.Date -- I am listing the columns because I want them to be arranged in a particular order
-FROM socialbuzz.CleanedReactions AS r
-JOIN socialbuzz.CleanedContent AS c -- Inner Join
-ON r.Content_ID = c.Content_ID) -- the subquery
+FROM socialbuzz.CleanedReactions AS r						--) the subquery
+JOIN socialbuzz.CleanedContent AS c -- Inner Join  
+ON r.Content_ID = c.Content_ID) 
 
 SELECT f.Content_ID, f.Category, f.Content_Type, f.Reaction_Type, t.Sentiment, t.Reaction_Score, f.Date -- The new query-- Listing the columns for same reason here. I could as well just say * for final_cte, but doesn't give me the order I want.
 FROM final_cte AS f
@@ -156,36 +154,8 @@ ON f.Reaction_Type = t.Reaction_Type
 Now that we have a final, merged dataset, the next task is to get the top 5 most popular content categories. 
 To achieve this, I need to perform an aggregation of total reaction score by content category, i.e, get the total reaction score for each category. 
 This can be done here in MSSQL using a simple SELECT statement plus the GROUPBY command.
-but I'll like to do this in a Spreadsheet, just to demonstrate my ability to work with different technologies (Excel or Google Sheet can be used. I'll be using Google Sheets- *wihispers* - my Excel access has expired)
-So, I will end here by exporting my view into a CSV file in preparation to continue in a Spreadsheet.
+but I'll like to do this in a Spreadsheet. This is just to demonstrate my ability to work with different technologies (Microsoft Excel or Google Sheet can be used. I'll be using Google Sheets)
+So, I will end here by exporting my view into a CSV file in preparation for analysis in a Spreadsheet.
 
 Thank you
 */
-
--- In Google sheet
--- First, In a new sheet, I used the UNIQUE() function to get a unique list of the categories.
--- I also used named ranges to make the ranges I am using in my analysis more descriptive.
---Here's my syntax
--- Without named ranges - UNIQUE(MergedSocialbuzzData!B2:B24574)
--- With named ranges- UNIQUE(category)
-
--- Then, I used 'SUMIF' to get the total reaction score for each category, with the below syntax
--- Without named ranges- =SUMIf(MergedSocialbuzzData!$B$2:$B$24574, A2, MergedSocialbuzzData!$F$2:$F$24574)
--- With named ranges- =SUMIf(category, A2, reactionscore)
-
--- And I used the Sort() function to sort in descending order
--- =SORT(A3:B18, B3:B18, False)
-
--- I am sharing the formulas here because you will not find any formula in my final spreadsheet. 
--- It is good practice to always copy and paste the results as values only after applying a formula or function, so it does not interfere in our analysis.
-
--- Also note that the aggregating and sorting process can also be achieved with pivot tables.
-
-/*Agenda - What will your presentation cover?
-Project Recap - What are the key points from the brief?
-Problem - What is the problem that you answer in this presentation?
-The Analytics team - Who is on your team?
-As a reminder from the earlier task - this includes: Andrew Fleming (Chief Technical Architect), Marcus Rompton (Senior Principle), and yourself!
-Process - How did you complete your analysis?
-*/
-
